11176befa44ffe28a2906db8ffe1f3e7a3e33b0b
Aatrox <335992583@qq.com>
2022-12-06 15:07:46 +0800

    1.使用session完成hmdp的登录注册以及验证过程

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获得session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户信息
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在,不存在则拦截
+        if(user == null){
+            response.setStatus(401);
+            return false;
+        }
+        //4.存在则保存到threadlocal中
+        UserHolder.saveUser((UserDTO) user);
+
+        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 没有，需要拦截，设置状态码
+            response.setStatus(401);
+            // 拦截
+            return false;
+        }
+        // 有用户，则放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
